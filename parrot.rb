8f6cc029f067e34a04d10d9eefbda447c78a084e
Camisha  Farris <cfarris@anointedflooring.com>
2020-01-25 06:08:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def #parrot (name)
- puts = "parrot, #{name}!"
+def parrot (name)
+ puts = "parrot, {name}!"
  end
  
  parrot ("Squawk!")
